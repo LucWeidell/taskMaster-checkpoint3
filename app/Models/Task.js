fa4850e413a477d85c553dbb51f4c8b47fa0dfb0
W1ndyWalker <60443786+W1ndyWalker@users.noreply.github.com>
2021-07-16 15:07:52 -0600

    task model written

--- app/Models/Task.js
+++ app/Models/Task.js
@@ -1,16 +1,27 @@
-
+import { generateId } from "../Utils/GeneratedID";
 
 export default class Task{
 
-  constructor({}){
+  constructor({taskJob}){
+    this.taskJob = taskJob;
 
-
+    this.taskID = generateId()
+    this.isSelected = false;
   }
 
+  flipSelect(){
+    this.isSelected = !this.isSelected;
+  }
 
   get Template() {
     return /*html*/`
-
+      <div class="col-md-12 d-flex justify-content-between">
+        <div class="form-group form-check" onchange="app.tasksController('${this.taskID}')" >
+          <input type="checkbox" class="form-check-input" id="testCheck">
+          <label class="form-check-label" for="testCheck">'${this.taskJob}'</label>
+        </div>
+        <span class="action" onclick="app.tasksController.deleteTask('${this.taskID}')">X</span>
+      </div>
 
 
 
